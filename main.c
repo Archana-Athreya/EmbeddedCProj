a4ca8b61583d9c5cdab8a7b16d9454d9b720212e
Archana-Athreya <79739203+Archana-Athreya@users.noreply.github.com>
2021-04-29 10:27:47 +0530

    Make file and main file

---
+++ main.c
@@ -0,0 +1,11 @@
+
+#include "activity1.h"
+#include "activity2.h"
+
+int main(void)
+{
+	init();/* Initialize Peripherals */
+	/* Turns LED ON if and only if both switches ButtonSensor and Heater are closed */
+	LED_actuatorstatus();
+	return 0;
+}
