6ea7a8f84e90214a31b281a06aa65ade660c7167
Archana-Athreya <79739203+Archana-Athreya@users.noreply.github.com>
2021-04-29 15:18:25 +0530

    Updated

--- main.c
+++ main.c
@@ -6,6 +6,6 @@ int main(void)
 {
 	init();/* Initialize Peripherals */
 	/* Turns LED ON if and only if both switches ButtonSensor and Heater are closed */
-	LED_actuatorstatus();
+	LED_actuatorstatus();/*Function inturn calls ADC,pwm and USART functions*/
 	return 0;
 }
