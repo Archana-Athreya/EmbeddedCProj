4769255911a35664e8aab01ef75048788a2b73db
Archana-Athreya <79739203+Archana-Athreya@users.noreply.github.com>
2021-04-29 10:29:28 +0530

    Update

--- src/Activity_3.c
+++
@@ -1,9 +0,0 @@
-#include "Activity_1.h"
-#include "Activity_3.h"
-
-void initPWM(void)
-{
-    TCCR1A=(1<<COM1A1)|(1<<WGM11)|(1<<WGM10);//using timer1
-    TCCR1B=(1<<WGM12)|(1<<CS11)|(1<<CS10);//64 prescale
-    DDRB|=(1<<PB1);
-}
